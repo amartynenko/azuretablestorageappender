a368417b601ecd41dac0eee9fe270b3d6082b2a9
Anton Martynenko <aka.makoed@gmail.com>
2013-10-15 15:19:55 +0300

    Migrating to Azure SDK 2.1

--- AzureTableStorageAppender.cs
+++ AzureTableStorageAppender.cs
@@ -5,44 +5,48 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
-	
+    http://www.apache.org/licenses/LICENSE-2.0
+    
 Unless required by applicable law or agreed to in writing, software 
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. 
  */
+
 using System.Data.Services.Client;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
 using log4net.Appender;
 using log4net.Core;
-using Microsoft.WindowsAzure;
+
 using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.StorageClient;
 
 namespace Demo.Log4Net.Azure
 {
     public class AzureTableStorageAppender : AppenderSkeleton
     {
         public string TableStorageConnectionStringName { get; set; }
-        private LogServiceContext _ctx;
-        private string _tableEndpoint;
+        private CloudTable table;
 
         public override void ActivateOptions()
         {
             base.ActivateOptions();
-            var cloudStorageAccount =
-                CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue(TableStorageConnectionStringName));
-            _tableEndpoint = cloudStorageAccount.TableEndpoint.AbsoluteUri;
-            CloudTableClient.CreateTablesFromModel(typeof(LogServiceContext), _tableEndpoint, cloudStorageAccount.Credentials);
-            _ctx = new LogServiceContext(cloudStorageAccount.TableEndpoint.AbsoluteUri, cloudStorageAccount.Credentials);
+
+            var storageAccount = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue(TableStorageConnectionStringName));
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            table = tableClient.GetTableReference("LogEntries");
+            table.CreateIfNotExists();
         }
 
         protected override void Append(LoggingEvent loggingEvent)
         {
             try
             {
-                _ctx.Log(new LogEntry
+                table.Execute(TableOperation.InsertOrReplace(new LogEntry
                 {
                     RoleInstance = RoleEnvironment.CurrentRoleInstance.Id,
                     DeploymentId = RoleEnvironment.DeploymentId,
@@ -53,12 +57,12 @@ namespace Demo.Log4Net.Azure
                     Domain = loggingEvent.Domain,
                     ThreadName = loggingEvent.ThreadName,
                     Identity = loggingEvent.Identity
-                });
+                }));
             }
             catch (DataServiceRequestException e)
             {
-                ErrorHandler.Error(string.Format("{0}: Could not write log entry to {1}: {2}",
-                    GetType().AssemblyQualifiedName, _tableEndpoint, e.Message));
+                ErrorHandler.Error(string.Format("{0}: Could not write log entry to Azure Table Storage: {1}",
+                    GetType().AssemblyQualifiedName, e.Message));
             }
         }
     }
--- LogEntry.cs
+++ LogEntry.cs
@@ -14,15 +14,17 @@ See the License for the specific language governing permissions and
 limitations under the License. 
  */
 using System;
-using Microsoft.WindowsAzure.StorageClient;
+
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Demo.Log4Net.Azure
 {
-    public class LogEntry : TableServiceEntity
+    public class LogEntry : TableEntity
     {
         public LogEntry()
         {
             var now = DateTime.UtcNow;
+
             PartitionKey = string.Format("{0:yyyy-MM}", now);
             RowKey = string.Format("{0:dd HH:mm:ss.fff}-{1}", now, Guid.NewGuid());
         }
